a747459c58cb438bb0a7811880b4df9d7f83ada1
emanuele sacchetti <manusacco92@gmail.com>
2023-05-30 16:14:51 +0200

    inizio struttura progetto in vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<script setup>
+<script>
+    import axios from 'axios';
+    import Header from '../src/components/Header.vue';
+    import AppMain from '../src/components/AppMain.vue';
 
+    export default {
+        components:{
+            Header,
+            AppMain
+        },
+    }
 </script>
 
 
 <template>
-
+    <Header></Header>
+    <AppMain></AppMain>
 </template>
 
 
 <style scoped>
-
+    @import './styles/general.scss'
 </style>
